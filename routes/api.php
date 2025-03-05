a09868e049abe8087a25f2e916c98618d17f00d4
Kyaw Thiha <kyaw.thiha@automatedpros.io>
2025-03-05 20:40:23 +0700

    mongodb setup migration and auth passport

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Api\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->name('login');
+
+Route::middleware(['auth:api'])->get('/user', function (Request $request) {
     return $request->user();
 });
